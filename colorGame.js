3a3860a6edb36eb1211660f467ec9ac98a77d853
Jei Chambers <jeilee88@outlook.com>
2019-06-18 02:43:25 -0500

    fixed color generator and tweaked correct guess effect

--- colorGame.js
+++ colorGame.js
@@ -3,7 +3,7 @@ var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector("#message");
-var h1 = document.querySelector("h1")
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -29,32 +29,32 @@ for (var i = 0; i < squares.length; i++){
 
 function changeColors(color){
     // loop through all squares 
-    for (var i = 0; i < colors.length; i++ ) {
+    for (var i = 0; i < squares.length; i++ ) {
         // change each color to match given color
         squares[i].style.background = color;
     }
 }
 
 function pickColor(){
-    var random = Math.floor(Math.random() * colors.length)
-    return colors[random]
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
 
 function generateRandomColors(num) {
-    arr = []
+    var arr = []
     // add num random colors to array
-    for (var i; i < num; i++){
-        arr.push(randomColor())
+    for (var i = 0; i < num; i++){
+        arr.push(randomColor());
     }
     return arr;
 }
 
 function randomColor(){
     // pick a red from 0 to 255
-    let r = Math.floor(Math.random() * 256)
+    let r = Math.floor(Math.random() * 256);
     // pick a green from 0 to 255
-    let g = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256);
     // pick a blue from 0 to 255
-    let b = Math.floor(Math.random() * 256)
-    return "rgb("+ r + ", " + g + ", " + b + ")";
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
